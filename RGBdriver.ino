933155fbf1546b18ac9945d27266f4a50788733b
PrzemcioM <37595395+PrzemcioM@users.noreply.github.com>
2018-07-26 09:15:43 +0200

    Update RGBdriver.ino
    Funkcja pora wywołuje warunki, wartosci zdefiniowane sa wartosciami testowym. Pozostają błędy przy ściemnianiu

--- RGBdriver.ino
+++ RGBdriver.ino
@@ -5,31 +5,31 @@
 
 //definicje godziny wschodu, podane w sekundach mierzonych od polnocy
 //np. wschod o 6.00 to 6*3600 + 0*60 = 21600
-#define wschod1 21600
-#define wschod2 21600
-#define wschod3 21600
+#define wschod1 15
+#define wschod2 15
+#define wschod3 15
 
 //definicje godziny poranka, podane w sekundach mierzonych od polnocy
 //np. dzien o 6.15 to 6*3600 + 15*60 = 22500
-#define dzien1  22500
-#define dzien2  22500
-#define dzien3  22500
+#define dzien1  30
+#define dzien2  30
+#define dzien3  30
 
 //definicje godziny zachodu, podane w sekundach mierzonych od polnocy
 //np. zachod o 20.00 to 22*3600 + 0*60 = 72000
-#define zachod1 72000
-#define zachod2 72000
-#define zachod3 72000
+#define zachod1 45
+#define zachod2 45
+#define zachod3 45
 
 //definicje godziny poranka, podane w sekundach mierzonych od polnocy
 //np. noc o 20.15 to 22*3600 + 15*60 = 72900
-#define noc1 72900
-#define noc2 72900
-#define noc3 72900
+#define noc1 60
+#define noc2 60
+#define noc3 60
 
 //// Maksymalna moc kanalu [%] //////
 #define maxPower1 30
-#define maxPower2 35
+#define maxPower2 30
 #define maxPower3 30
 
 struct czas{
@@ -38,6 +38,8 @@ struct czas{
 };
 struct czas aktualny;
 
+int rozciagniecie;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(pwm1, OUTPUT);
@@ -45,60 +47,45 @@ void setup() {
   pinMode(pwm3, OUTPUT);
 }
 
-void loop() {
-  delay(1);   
+void loop() {   
   zegar(&aktualny);
-  pora(&aktualny);  
-  
+  pora(&aktualny, pwm1, maxPower1, wschod1, dzien1, zachod1, noc1);
+  pora(&aktualny, pwm2, maxPower2, wschod2, dzien2, zachod2, noc2);
+  pora(&aktualny, pwm3, maxPower3, wschod3, dzien3, zachod3, noc3);
 }
 
-//3 kanalowy softPWM. Przepisuje na 1 kanałowy
-void softPWM(int kanal, int wypelnienie, bool kierunek){
-    for( int i = 0 ; i < 1000 ; i++){
-      if( i < wypelnienie)
-        digitalWrite(kanal,kierunek);
-      else 
-        digitalWrite(kanal,!kierunek);
-    }
-}
-
+bool stan = LOW;
 void zegar(struct czas *t){
-   t->milisekundy = (t->milisekundy)+1;
-   
-   if ( t->milisekundy == 999){         //sekunda ma 1000 milisekund
-      t->sekundy++;
-      t->milisekundy = 0;
-   }
-  
-   if ( t->sekundy == 86400){           //doba 86400 sekund
-    t->sekundy = 0;
-   }
+  delay(1);
+  t->milisekundy++;
+  if(t->milisekundy==999){
+    t->milisekundy=0;
+    t->sekundy++;
+  }
+  if(aktualny.sekundy == 86400){           //doba 86400 sekund
+    aktualny.sekundy = 0;
+  }
 }
 
-void pora(struct czas *t){
-   switch(t->sekundy){
-    case wschod1:
-      wschodzi(&aktualny, pwm1);
-    break;
-    
-    case dzien1:
-      analogWrite(pwm1,maxPower1);
-    break;
-    
-    case zachod1: 
-      //zachodzi(&aktualny, pwm1);
-    break;
-    
-    case noc1:
-      digitalWrite(pwm1,LOW);
-    break;
-    
-    default:
-      digitalWrite(pwm1,LOW);
-   }   
+void pora(struct czas *t, int pwm, int maxPower, int wschod, int dzien, int zachod, int noc){
+  if(t->sekundy < wschod) {
+    wschodzi(&aktualny, pwm, maxPower, wschod, dzien);
+  }  
+  if( dzien < t->sekundy) {
+    analogWrite(pwm, maxPower);
+  }
+  if(zachod < t->sekundy) {
+    zachodzi(&aktualny, pwm, maxPower, zachod, noc);
+  }
+
+  //to przypisane czasu potrzebne jest tylko do testu, zeby skrocic petle
+  if(noc < t->sekundy) {
+    digitalWrite(pwm, LOW);
+ //   aktualny.sekundy = 0; //--------------------//
+  }  
 }
 
-void wschodzi(struct czas *t, int kanal, int maxPower, int dzien, int wschod){
+void wschodzi(struct czas *t, int kanal, int maxPower, int wschod, int dzien) {
   int kierunek = HIGH;
   if (t->milisekundy == 0)
     rozciagniecie++; 
@@ -108,8 +95,17 @@ void wschodzi(struct czas *t, int kanal, int maxPower, int dzien, int wschod){
 
 void zachodzi(struct czas *t, int kanal, int maxPower, int noc, int zachod){
   int kierunek = LOW;
-  if (t->milisekundy == 0)
+  if(t->milisekundy == 0)
     rozciagniecie++; 
   int tmp = (maxPower / (noc-zachod))*rozciagniecie;
   softPWM(kanal, tmp, kierunek);
+}
+
+void softPWM(int kanal, int wypelnienie, bool kierunek){
+    for( int i = 0 ; i < 1000 ; i++) {
+      if( i < wypelnienie)
+        digitalWrite(kanal,kierunek);
+      else 
+        digitalWrite(kanal,!kierunek);
+    }
 }
